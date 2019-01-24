25c5063fe531a73447b4aca62c22289dabacc582
JiayuanDeng <JiayuanDeng0109@yahoo.com>
2019-01-24 15:11:33 -0800

    Updates

---
+++ precache-manifest.31eaf0a2d43fa106a8af91a22cae833b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "616292260aeec8b33b82",
+    "url": "./static/js/main.61629226.chunk.js"
+  },
+  {
+    "revision": "3c8499b7a62f483ae83c",
+    "url": "./static/js/1.3c8499b7.chunk.js"
+  },
+  {
+    "revision": "616292260aeec8b33b82",
+    "url": "./static/css/main.cd1789d3.chunk.css"
+  },
+  {
+    "revision": "7c65250d9f36484000d79d85e0497758",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.44825318f8702cab7e582a8b15a2fce6.js"
+  "./precache-manifest.31eaf0a2d43fa106a8af91a22cae833b.js"
 );
 
 workbox.clientsClaim();
