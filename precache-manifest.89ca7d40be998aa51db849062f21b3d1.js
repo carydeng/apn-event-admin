355b78360372409e63437db16d760f50746f50f7
JiayuanDeng <JiayuanDeng0109@yahoo.com>
2019-01-23 19:50:13 -0800

    Updates

---
+++ precache-manifest.89ca7d40be998aa51db849062f21b3d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/apn-event-admin/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "00bb37b17c0d5df4c10a",
+    "url": "/apn-event-admin/static/js/runtime~main.00bb37b1.js"
+  },
+  {
+    "revision": "c6b1c970a4fa96762ac9",
+    "url": "/apn-event-admin/static/js/main.c6b1c970.chunk.js"
+  },
+  {
+    "revision": "16fc75811f6c529108e7",
+    "url": "/apn-event-admin/static/js/1.16fc7581.chunk.js"
+  },
+  {
+    "revision": "c6b1c970a4fa96762ac9",
+    "url": "/apn-event-admin/static/css/main.b357046e.chunk.css"
+  },
+  {
+    "revision": "351812e306e38c5fb1ff72ae3adb2563",
+    "url": "/apn-event-admin/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ed23bd0af35168c1330ae5253abd2ad7.js"
+  "/apn-event-admin/precache-manifest.89ca7d40be998aa51db849062f21b3d1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/apn-event-admin/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
